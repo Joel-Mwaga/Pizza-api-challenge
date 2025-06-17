4d131695c83f82781f73b6240702f03226975483
Joel Mwaga <mwagahjoel14@gmail.com>
2025-06-17 12:35:16 +0300

    building the app.py

--- server/app.py
+++ server/app.py
@@ -1,18 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from server.config import Config
-from server.controllers.restaurant_controller import restaurant_bp
-from server.controllers.pizza_controller import pizza_bp
-from server.controllers.restaurant_pizza_controller import restaurant_pizza_bp
+from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+migrate = Migrate()
 
-# Register Blueprints
-app.register_blueprint(restaurant_bp)
-app.register_blueprint(pizza_bp)
-app.register_blueprint(restaurant_pizza_bp)
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pizza.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register blueprints here
+    from server.controllers.restaurant_controller import bp as restaurant_bp
+    from server.controllers.pizza_controller import bp as pizza_bp
+    from server.controllers.restaurant_pizza_controller import bp as restaurant_pizza_bp
+
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(restaurant_pizza_bp)
+
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
